9d261307ecfc2f82195d4578ded252d6851e79d3
publicFunction <chris@publicfunction.co.uk>
2013-06-02 20:33:51 +0100

    base rails app
    Signed-off-by: publicFunction <chris@publicfunction.co.uk>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '2595f7a304ea07a8be1bf437545f1910427cd8603593b541c09859c7246d131daf5e5b8eecd33786093d40a2ecb7fe6e5abc3cc3a3e0894116b98f95d9afe43d'
